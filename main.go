ffe7f9a707d3c3702719657ffb801fb7f963697c
Tim Sogard <src+grayce@timsogard.com>
2020-05-16 16:14:45 -0400

    Accept port from heroku runtime

--- main.go
+++ main.go
@@ -1,16 +1,22 @@
 package main
 
-import "fmt"
-import "log"
-import "net/http"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
 
 func main() {
-	fmt.Println("vim-go")
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
 
 	helloHandler := func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "henlo")
 	}
-
 	http.HandleFunc("/hello", helloHandler)
-	log.Fatal(http.ListenAndServe(":8111", nil))
+
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
